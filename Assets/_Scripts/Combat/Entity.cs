f7f6eb7cc388cb6761fb3b15848699d75fcb8214
nathan-exe <nathan.tazi.1@gmail.com>
2025-01-23 11:30:06 +0100

    debut data

--- Assets/_Scripts/Combat/Entity.cs
+++ Assets/_Scripts/Combat/Entity.cs
@@ -1,8 +1,22 @@
+using Cysharp.Threading.Tasks;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Entity : MonoBehaviour
 {
-   // public async Uniitask
+    public float HP { get; private set; }
+
+    [Header("References")]
+    [SerializeField] private EntityData _data;
+
+    public void Init()
+    {
+        
+    }
+    public async UniTask PlayTurn()
+    {
+
+    }
+    
 }
