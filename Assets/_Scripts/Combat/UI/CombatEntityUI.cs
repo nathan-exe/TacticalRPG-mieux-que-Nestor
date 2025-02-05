9c4d3c8a261c4b4efe70aa2ae0936a944ab11031
Chorizo6969 <146953802+Chorizo6969@users.noreply.github.com>
2025-02-05 18:57:36 +0100

    Fin des transition entre overworld et fight

--- Assets/_Scripts/Combat/UI/CombatEntityUI.cs
+++ Assets/_Scripts/Combat/UI/CombatEntityUI.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CombatEntityUI : MonoBehaviour
@@ -7,6 +8,7 @@ public class CombatEntityUI : MonoBehaviour
     [Header("References")]
     public CoolSlider HealthSlider;
     public CoolSlider ManaSlider;
+    public TextMeshProUGUI Name;
     public void PreviewManaLoss(float loss) => ManaSlider.PreviewValue(ManaSlider.Value - loss);
     public void CancelManaLossPreview() => ManaSlider.CancelPreview();
 
