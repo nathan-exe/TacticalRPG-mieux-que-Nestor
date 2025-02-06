1d5f22a768de1ef188738af40b55a94dd65790b6
Emrys <legall.emrys@gmail.com>
2025-02-06 10:14:29 +0100

    "aqzeshjoiuytrfdychvbnkm

--- Assets/_Scripts/Overworld/Interactables/Implementation/Interactible.cs
+++ Assets/_Scripts/Overworld/Interactables/Implementation/Interactible.cs
@@ -4,18 +4,18 @@ using System.Collections.Generic;
 using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
 
-public class Sign : MonoBehaviour, Iinteractable
+public class Interactible : MonoBehaviour, Iinteractable
 {
 
     public string signName = "Sign";
-    [TextArea] public string signText;
+    [TextArea] 
+    public string signText;
     
     public async UniTask InteractWith()
     {
         Debug.Log("coucou C moi");
         await DialogueBoxDisplay.Instance.OpenDialogueBox(signName, signText);
         await DialogueBoxDisplay.Instance.IsSkipping();
-
     }
 
     public void OnSelected()
---
+++ Assets/_Scripts/Overworld/Player/PlayerPartyOverworld.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPartyOverworld : MonoBehaviour
+{
+    public static List<PlayerPartyOverworld> Instances = new List<PlayerPartyOverworld>();
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
